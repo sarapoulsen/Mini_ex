b8e6dbfb9e05e6ff7a9a609d780db1c54e9b6cd4
sarapoulsen <36232535+sarapoulsen@users.noreply.github.com>
2018-02-09 10:58:23 +0100

    Add files via upload

---
+++ mini-ex1/mini_ex1/empty-example/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  // put setup code here
+  createCanvas(500,500);
+  console.log("heeeej");
+}
+
+function draw() {
+  // put drawing code here
+  background(255, 204, 0);
+  ellipse(70,70,100,100);
+  rect(60, 50, 100, 100);
+  textSize(20);
+textAlign(RIGHT);
+text('nu har jeg lavet', 200, 30);
+textAlign(CENTER);
+text('EN KODE', 50, 50);
+textAlign(LEFT);
+text('what det er cool', 80, 70);
+}
